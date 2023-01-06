fde4a71415561fc494c74a0a0251bfd9a7d01786
martinlaw <martin.law@mrc-bsu.cam.ac.uk>
2023-01-06 17:15:52 +0000

    Completed all analyses, created webpages

--- R/load.R
+++ R/load.R
@@ -1,5 +1,5 @@
 library(librarian)
-librarian::shelf(here, readxl, metafor)
+librarian::shelf(here, readxl, metafor, martinlaw/dex)
 
 #### Load data ####
 dat <- read_excel(here("data", "Dex_metan.xlsx"), range = "B2:T11")
@@ -11,7 +11,7 @@ names(dat) <- c("study", "n1i", "n2i",
                 )
 dat
 
-# First problem: there is no data for Lee et al. (final row)
+# There is no data for Lee et al. (final row)
 
 
 #### Primary Analyses ####
@@ -117,3 +117,89 @@ names(dat.second) <- c("study",
                        "n1i", "n2i",
                        "outcome")
 # Plateau pressure:
+dat.plat <- escalc(measure="MD",
+                   m1i=m1i,
+                   sd1i=sd1i,
+                   n1i=n1i,
+                   m2i=m2i,
+                   sd2i=sd2i,
+                   n2i=n2i,
+                   data=dplyr::filter(dat.second, outcome=="Plateau Pressure"),
+                   slab = study)
+
+pm.plat <- rma(yi, vi, data=dat.plat, method="PM")
+forest(pm.plat,
+       at=seq(from=-8, to =2, by=2))
+
+# Peak pressure:
+dat.peak <- escalc(measure="MD",
+                   m1i=m1i,
+                   sd1i=sd1i,
+                   n1i=n1i,
+                   m2i=m2i,
+                   sd2i=sd2i,
+                   n2i=n2i,
+                   data=dplyr::filter(dat.second, outcome=="Peak Pressure"),
+                   slab = study)
+
+pm.peak <- rma(yi, vi, data=dat.peak, method="PM")
+forest(pm.peak,
+       at=seq(from=-8, to =2, by=2))
+
+# Respiratory Compliance:
+dat.resp <- escalc(measure="MD",
+                   m1i=m1i,
+                   sd1i=sd1i,
+                   n1i=n1i,
+                   m2i=m2i,
+                   sd2i=sd2i,
+                   n2i=n2i,
+                   data=dplyr::filter(dat.second, outcome=="Respiratory Compliance"),
+                   slab = study)
+
+pm.resp <- rma(yi, vi, data=dat.resp, method="PM")
+forest(pm.resp,
+       at=seq(from=0, to =8, by=2))
+
+
+# POD1:
+dat.pod1<- escalc(measure="MD",
+                   m1i=m1i,
+                   sd1i=sd1i,
+                   n1i=n1i,
+                   m2i=m2i,
+                   sd2i=sd2i,
+                   n2i=n2i,
+                   data=dplyr::filter(dat.second, outcome=="FEV1 POD 1"),
+                   slab = study)
+
+pm.pod1 <- rma(yi, vi, data=dat.pod1, method="PM")
+forest(pm.pod1)
+
+# POD2:
+dat.pod2<- escalc(measure="MD",
+                  m1i=m1i,
+                  sd1i=sd1i,
+                  n1i=n1i,
+                  m2i=m2i,
+                  sd2i=sd2i,
+                  n2i=n2i,
+                  data=dplyr::filter(dat.second, outcome=="FEV1 POD 2"),
+                  slab = study)
+
+pm.pod2 <- rma(yi, vi, data=dat.pod2, method="PM")
+forest(pm.pod2)
+
+
+#### Funnel plots ####
+
+### carry out trim-and-fill analysis
+taf.atelectasis <- trimfill(pm.atelectasis)
+### draw funnel plot with missing studies filled in
+funnel(taf.atelectasis, legend=TRUE)
+
+taf.pneumonia <- trimfill(pm.pneumonia)
+funnel(taf.pneumonia, legend=TRUE)
+
+
+#usethis::use_vignette("Secondary_outcomes")
